238ee8f1df86da2b933f27aed0d7907518609d44
RobertoMiranda <rmp0046@gmail.com>
2015-06-03 17:59:01 +0200

    #3 Creación de las clases WorkStation y Printer, que heredan de Node

---
+++ src/lanSimulation/internals/Printer.java
@@ -0,0 +1,10 @@
+package lanSimulation.internals;
+
+public class Printer extends Node{
+	public static final byte PRINTER = 2;
+	public Printer(String name) {
+		// TODO Auto-generated constructor stub
+		super(PRINTER, name);
+	}
+
+}
---
+++ src/lanSimulation/internals/WorkStation.java
@@ -0,0 +1,12 @@
+package lanSimulation.internals;
+
+public class WorkStation extends Node{
+	
+	public static final byte PRINTER = 2;
+	
+	public WorkStation(String name) {
+
+		super(PRINTER, name);
+	}
+
+}
